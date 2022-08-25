7b09f485d610312fb3c603457338b1a43e665888
venkatamanyam <kvsubbu.db2@gmail.com>
2022-08-26 01:48:30 +0530

    26812

---
+++ demos-34/src/main/java/com/in/Demos34Application.java
@@ -0,0 +1,13 @@
+package com.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demos34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demos34Application.class, args);
+	}
+
+}
