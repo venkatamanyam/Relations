8ff5337d44473fc93fb3ec6e19f4cd6abb23c5b2
venkatamanyam <kvsubbu.db2@gmail.com>
2022-08-26 01:25:12 +0530

    2684s

---
+++ demos-33/src/main/java/com/in/Demos33Application.java
@@ -0,0 +1,13 @@
+package com.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demos33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demos33Application.class, args);
+	}
+
+}
---
+++ demos-33/src/main/java/com/in/base/B.java
@@ -0,0 +1,9 @@
+package com.in.base;
+
+public class B {
+
+	public void m2()
+	{
+		System.out.println("hi");
+	}
+}
